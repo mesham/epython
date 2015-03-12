ec8a539ea0d59e3ab69ad1e933d57f65e5fb37a2
Nick <nick.brown@ed.ac.uk>
2015-03-12 15:40:46 +0000

    Removed code shared from earlier OS versions as the reasons behind this are now fixed

--- ebasic.sh
+++ ebasic.sh
@@ -22,5 +22,5 @@ if [[ "$OS_VER" -ge "14" || "$OS_MAJ" -gt "3" ]]
 then
 $FILE "$@"
 else
-sudo -E LD_LIBRARY_PATH=${ELIBS} EPIPHANY_HDF=${EHDF} $FILE -codeshared "$@"
+sudo -E LD_LIBRARY_PATH=${ELIBS} EPIPHANY_HDF=${EHDF} $FILE "$@"
 fi
