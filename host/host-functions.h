23d6da22e17732b6bec2184623f9241d69b611e6
Nick <nick.brown@ed.ac.uk>
2015-04-25 09:35:22 +0100

    Host functions header file

---
+++ host/host-functions.h
@@ -0,0 +1,13 @@
+/*
+ * host-functions.h
+ *
+ *  Created on: 8 Apr 2015
+ *      Author: nick
+ */
+
+#ifndef HOST_FUNCTIONS_H_
+#define HOST_FUNCTIONS_H_
+
+void initHostCommunicationData(int);
+
+#endif /* HOST_FUNCTIONS_H_ */
