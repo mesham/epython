6a29d902ef657883d36b942e58a02d7dbc88790f
Nick <nick.brown@ed.ac.uk>
2016-05-25 15:51:28 +0100

    Bug fix, range was overwriting the input values

--- util.py
+++ util.py
@@ -1,11 +1,13 @@
 def range(a,b=none):
+	i_a=a
+	i_b=b
 	if (b is none):
-		b=a
-		a=0
-	dim r[(b-a)+1]
-	i=a
-	while i<=b:
-		r[i-a]=i
+		i_b=a
+		i_a=0
+	dim r[(i_b-i_a)+1]
+	i=i_a
+	while i<=i_b:
+		r[i-i_a]=i
 		i+=1
 	return r
 
