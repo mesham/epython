a4f43deb00696ace0a05fdfd781d8b0ff15e3395
Nick <nick.brown@ed.ac.uk>
2016-06-14 21:19:31 +0100

    Range supports increment range odd-even sort example

---
+++ examples/odd-even-sort.py
@@ -0,0 +1,28 @@
+/*
+Simple odd-even sort
+*/
+
+import util
+
+dim x[100]
+for i in range(99):
+	x[i]=random%100
+
+sorted=false
+while not sorted:
+	sorted=true
+	for i in range(0, len(x)-1, 2):
+		if x[i] > x[i+1]:
+			temp= x[i]
+			x[i]=x[i+1]
+			x[i+1] = temp
+			sorted=false
+	for i in range(1, len(x)-2, 2):
+		if x[i] > x[i+1]:
+			temp= x[i]
+			x[i]=x[i+1]
+			x[i+1] = temp
+			sorted=false
+
+for i in range(99):
+	print x[i]
--- util.py
+++ util.py
@@ -1,14 +1,22 @@
-def range(a,b=none):
+def range(a,b=none,c=none):
 	i_a=a
 	i_b=b
+	i_c=c
 	if (b is none):
 		i_b=a
 		i_a=0
-	dim r[(i_b-i_a)+1]
+	if (c is none):
+		i_c=1
+	dim r[((i_b-i_a)/i_c)+1]
 	i=i_a
+	j=0
 	while i<=i_b:
-		r[i-i_a]=i
-		i+=1
+		r[j]=i
+		if (c is none):
+			i+=1
+		else:
+			i=i+c
+		j=j+1
 	return r
 
 def xrange(a,b):
