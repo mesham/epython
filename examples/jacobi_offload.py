f55c2e86f603475b9408a49e9dfd3d002be306d2
Nick Brown <nick@mesham.com>
2017-03-20 20:30:05 +0000

    Jacobi offload example

---
+++ examples/jacobi_offload.py
@@ -0,0 +1,57 @@
+from epython import *
+import array
+import math
+
+data=None
+data_p1=None
+define_on_epiphany(data)
+define_on_epiphany(data_p1)
+
+MAX_ITS=10000
+
+@epiphany
+def initialise(global_size):
+  num_local=global_size/(numcores()-1)
+  if num_local * (numcores()-1) != global_size:
+    if (coreid() < global_size-num_local*(numcores()-1)): num_local+=1
+  data=[0.0]*(num_local+2)
+  data_p1=[0.0]*(num_local+2)
+  if coreid()==0: data[0]=1.0
+  if coreid()==numcores()-2: data[num_local+1]=10.0
+
+@epiphany
+def calc_residual():
+  tmpnorm=0.0
+  i=1
+  while i<=len(data)-2:
+    tmpnorm=tmpnorm+(data[i]*2-data[i-1]-data[i+1])^2
+    i+=1
+  return tmpnorm
+
+@epiphany
+def jacobi_iteration():
+  if (coreid() > 0): data[0]=sendrecv(data[1], coreid()-1)
+  if (coreid() < numcores()-2): data[len(data)-1]=sendrecv(data[len(data)-2], coreid()+1)
+  i=1
+  while i<=len(data)-2:
+    data_p1[i]=0.5* (data[i-1] + data[i+1])
+    i+=1
+  # Swap data around for next iteration
+  i=1
+  while i<=len(data)-2:
+    data[i]=data_p1[i]
+    i+=1
+
+initialise(100)
+a=calc_residual()
+bnorm=math.sqrt(sum(a))
+
+norm=1.0
+it=0
+
+while norm > 1e-4:
+  jacobi_iteration()
+  rn=calc_residual()
+  norm=math.sqrt(sum(rn))/bnorm
+  it+=1
+  if it%100 == 0 : print "Rnorm is "+str(norm)+" after "+str(it)+" iterations"
