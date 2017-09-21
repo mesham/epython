8837e02d36045d6e41b4fde83f45a37cf04948e8
The Octane Group <contact@theoctanegroup.uk>
2017-09-21 12:46:00 +0000

    Updated jacobi offload example

--- examples/jacobi_offload.py
+++ examples/jacobi_offload.py
@@ -4,12 +4,12 @@ import math
 
 data=None
 data_p1=None
-define_on_epiphany(data)
-define_on_epiphany(data_p1)
+define_on_device(data)
+define_on_device(data_p1)
 
 MAX_ITS=10000
 
-@epiphany
+@offload
 def initialise(global_size):
   num_local=global_size/(numcores()-1)
   if num_local * (numcores()-1) != global_size:
@@ -19,7 +19,7 @@ def initialise(global_size):
   if coreid()==0: data[0]=1.0
   if coreid()==numcores()-2: data[num_local+1]=10.0
 
-@epiphany
+@offload
 def calc_residual():
   tmpnorm=0.0
   i=1
@@ -28,7 +28,7 @@ def calc_residual():
     i+=1
   return tmpnorm
 
-@epiphany
+@offload
 def jacobi_iteration():
   if (coreid() > 0): data[0]=sendrecv(data[1], coreid()-1)
   if (coreid() < numcores()-2): data[len(data)-1]=sendrecv(data[len(data)-2], coreid()+1)
@@ -54,4 +54,4 @@ while norm > 1e-4:
   rn=calc_residual()
   norm=math.sqrt(sum(rn))/bnorm
   it+=1
-  if it%100 == 0 : print "Rnorm is "+str(norm)+" after "+str(it)+" iterations"
+  if it%50 == 0 : print "Rnorm is "+str(norm)+" after "+str(it)+" iterations"
